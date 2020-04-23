d44a8b1e754a69397fea2a02679146a9f14fd8f6
jgilson <jose.gilson@watchguard.com>
2020-04-23 09:30:45 -0300

    Upload base project files

---
+++ src/native-test/java/com/jjdev/NativeExampleIT.java
@@ -0,0 +1,9 @@
+package com.jjdev;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeExampleIT extends ExampleTest {
+
+    // Execute the same tests but in native mode.
+}
